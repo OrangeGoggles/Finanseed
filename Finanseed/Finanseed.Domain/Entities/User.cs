f2a92f081082db1e87877817172fbad3b8de6c2b
Vinícius Campos <vinicius.campos@squadra.com.br>
2017-12-05 18:22:38 -0200

    Commit do Teste

--- Finanseed/Finanseed.Domain/Entities/User.cs
+++ Finanseed/Finanseed.Domain/Entities/User.cs
@@ -1,25 +1,73 @@
-﻿using Finanseed.Domain.ValueObject;
-using System;
+﻿using System;
 using System.Collections.Generic;
 
 namespace Finanseed.Domain.Entities
 {
     public class User : EntityBase
     {
-        public User()
+        public User(DateTime birthday, string email, string passwordHash, string phoneNumber, string userName)
         {
+            Birthday = birthday;
+            Email = email;
+            PasswordHash = passwordHash;
+            PhoneNumber = phoneNumber;
+            UserName = userName;
             ID = Guid.NewGuid();
-            base.CreationDate = DateTime.Now;
+            CreationDate = DateTime.Now;
+            Wallets = new List<Wallet>();
+            EmailConfirmed = false;
+            PhoneNumberConfirmed = false;
+            AccessFailedCount = 0;
+            Active = false;
         }
+
         public Guid ID { get; set; }
+
         public DateTime Birthday { get; set; }
-        public int AccessFailedCount { get; set; }
         public string Email { get; set; }
-        public bool EmailConfirmed { get; set; }
         public string PasswordHash { get; set; }
         public string PhoneNumber { get; set; }
-        public bool PhoneNumberConfirmed { get; set; }
         public string UserName { get; set; }
+
         public virtual ICollection<Wallet> Wallets { get; set; }
+
+        public bool EmailConfirmed { get; set; }
+        public bool PhoneNumberConfirmed { get; set; }
+        public int AccessFailedCount { get; set; }
+        public bool Active { get; private set; }
+
+        public void ConfirmEmail()
+        {
+            EmailConfirmed = true;
+        }
+
+        public void ConfirmPhoneNumber()
+        {
+            PhoneNumberConfirmed = true;
+        }
+
+        public void AccessFailed()
+        {
+            if (AccessFailedCount < 3)
+                AccessFailedCount++;
+            else
+                Deactivate();
+        }
+
+        public void Activate()
+        {
+            Active = true;
+        }
+
+        private void Deactivate()
+        {
+            Active = false;
+        }
+
+        public void AddWallet(Wallet wallet)
+        {
+            wallet.OwnerID = ID;
+            Wallets.Add(wallet);
+        }
     }
 }
---
+++ Finanseed/Finanseed.DomainTests/Entities/UserTests.cs
@@ -0,0 +1,35 @@
+﻿using Finanseed.Domain.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Finanseed.Domain.Entities.Tests
+{
+    [TestClass()]
+    public class UserTests
+    {
+        [TestMethod()]
+        public void UserTest()
+        {
+            var user = new User(new DateTime(1995, 10, 11), "vpcmps@gmail.com", "vpcmps", "+5561994544774", "Vinicius Campos");
+            Assert.IsFalse(user.Active);
+            Assert.IsFalse(user.EmailConfirmed);
+            Assert.IsFalse(user.PhoneNumberConfirmed);
+        }
+
+        [TestMethod()]
+        public void ConfirmEmailTest()
+        {
+            var user = new User(new DateTime(1995, 10, 11), "vpcmps@gmail.com", "vpcmps", "+5561994544774", "Vinicius Campos");
+            user.ConfirmEmail();
+            Assert.IsTrue(user.EmailConfirmed);
+        }
+
+        [TestMethod()]
+        public void ConfirmPhoneNumberTest()
+        {
+            var user = new User(new DateTime(1995, 10, 11), "vpcmps@gmail.com", "vpcmps", "+5561994544774", "Vinicius Campos");
+            user.ConfirmPhoneNumber();
+            Assert.IsTrue(user.PhoneNumberConfirmed);
+        }
+    }
+}
